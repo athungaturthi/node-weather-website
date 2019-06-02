4b877bae01b7278acfbe9c7deba349577190dfb5
Ashwini <ashwinithungaturthi@gmail.com>
2019-06-02 11:53:49 -0400

    Added High and low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('unable to find location', undefined)
         } else {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + 
-            body.currently.temperature + ' degrees out. There is a ' 
+            body.currently.temperature + ' degrees out. The High today is '+ body.daily.data[0].temperatureHigh + ' and low is ' + body.daily.data[0].temperatureLow +' There is a ' 
                 + body.currently.precipProbability +  '% chance of rain.')            
         }
     })
